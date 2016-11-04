cd0b4a112e6ec0e71a54ca3beb94886f6efc1575
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-11-04 16:06:59 +0200

    feat(renderer): added hack for prettify js loader

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var marked = require("marked");
 var loaderUtils = require("loader-utils");
 var assign = require("object-assign");
 
+// small but needed hack
+marked.Renderer.prototype.code = function renderCode(code, lang) {
+    const escCode = code.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+    if (!lang) {
+        return `<pre class="prettyprint">${escCode}</pre>`;
+    }
+    return `<pre class="prettyprint lang-${lang}">${escCode}</pre>`;
+};
+
 // default option
 var options = {
     renderer: new marked.Renderer(),
